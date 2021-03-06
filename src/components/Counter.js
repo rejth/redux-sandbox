fc06ea87de97237cf9ad88b7542a36b5474e182c
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-03-06 22:33:56 +0300

    fix eslintrc file

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 // функция для связывания action creators с методом store.dispatch() для отправки в reducer
 import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 // action creators
 import * as actions from '../actions';
 
@@ -22,21 +23,26 @@ const Counter = ({ count, inc, dec }) => (
 //property count = state from redux-store
 const mapStateToProps = state => ({ count: state });
 
+// связываем функции action creators с dispatch, bindActionCreators возвращает объект {}
+// созданные таким образом функции делают сразу 2 действия:
+// 1. создают action { type: ..., ...}
+// 2. передают action в метод store.dispatch - dispatch({ type: ..., ...}) или dispatch(creator())
+// таким образом мы изолируем View Layer от логики Redux,
+// что позволяет легко масштабировать View Layer независимо от того,
+// используем мы Redux или любую другую библиотеку управления состоянием компонентов
+// React-компонеты должны быть как можно меньше связаны с Redux
 //properties inc, dec = dispatched actions inc, dec from redux-store
-const mapDispatchToProps = dispatch => {
-  // связываем функции action creators с dispatch, bindActionCreators возвращает объект {}
-  // созданные таким образом функции делают сразу 2 действия:
-  // 1. создают action { type: ..., ...}
-  // 2. передают action в метод store.dispatch - dispatch({ type: ..., ...}) или dispatch(creator())
-  // таким образом мы изолируем View Layer от логики Redux,
-  // что позволяет легко масштабировать View Layer независимо от того,
-  // используем мы Redux или любую другую библиотеку управления состоянием компонентов
-  // React-компонеты должны быть как можно меньше связаны с Redux
-  return bindActionCreators(actions, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+Counter.propTypes = {
+  count: PropTypes.number.isRequired,
+  inc: PropTypes.func,
+  dec: PropTypes.func,
 };
 
 // функция connect() вернет новый компонент, который оборачивает Counter
 // этот компонент-обертка обеспечивает связь Counter с Redux
 // т.е он имеет доступ к store и передает state и actions из store в Counter для рендеринга
-// вместо mapDispatchToProps можно передать просто actions, затем connect() сам применит bindActionCreators(actions, dispatch)
+// вместо mapDispatchToProps можно передать просто actions, затем connect()
+// сам применит bindActionCreators(actions, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
